5e0b324835616f78e28a02967b28740171b2548d
Azhovan Asadi <azhovan@one.fit>
2019-12-30 14:22:48 +0100

    update algorith to support un-ordered sets (x,y) and (y,x)

--- array/min-distance-between-numbers.go
+++ array/min-distance-between-numbers.go
@@ -16,25 +16,34 @@ import (
 //Input: arr[] = {3, 4, 5}, x = 3, y = 5
 //Output: Minimum distance between 3 and 5 is 2.
 
+// Input: arr[] = {3, 5, 4, 2, 6, 5, 6, 6, 5, 4, 8, 3}, x = 3, y = 6
+//Output: Minimum distance between 3 and 6 is 4.
+//
+//Input: arr[] = {2, 5, 3, 5, 4, 4, 2, 3}, x = 3, y = 2
+//Output: Minimum distance between 3 and 2 is 1.
+
+// Notice that order is NOT important means (x,y) and (y,x) are the case
+// Time complexity O(n)
 func MinDistanceBetweenNumbers(A []int, x, y int) int {
 	minCount := math.MaxInt64
 	currentCount := 0
 	found := false
-
+	start := math.MinInt64
 	for i := 0; i < len(A); i++ {
-		if A[i] == x {
+		if (A[i] == x || A[i] == y ) && found == false {
 			found = true
+			start = A[i]
 		}
-		if found && A[i] != y {
+		if found == true  && A[i] != start{
 			currentCount++
 		}
-
-		if A[i] == y {
+		if (A[i] == x || A[i] == y) && A[i] != start {
 			if minCount > currentCount {
 				minCount = currentCount
-				currentCount = 0
-				found = false
 			}
+			currentCount = 0
+			start = math.MinInt64
+			found = false
 		}
 	}
 	return minCount
